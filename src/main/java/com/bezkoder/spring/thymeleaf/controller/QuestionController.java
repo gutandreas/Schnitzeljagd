763803ca50ff38a7b03620b06d071a7105bccd72
Andreas Gut <andreasgut@gmx.ch>
2023-05-19 21:37:42 +0200

    Postennummern werden aktualisiert (erst das erste Mal...)

--- src/main/java/com/bezkoder/spring/thymeleaf/controller/QuestionController.java
+++ src/main/java/com/bezkoder/spring/thymeleaf/controller/QuestionController.java
@@ -2,7 +2,6 @@ package com.bezkoder.spring.thymeleaf.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import com.bezkoder.spring.thymeleaf.Question;
 import com.bezkoder.spring.thymeleaf.QuestionList;
--- src/main/java/com/bezkoder/spring/thymeleaf/entity/User.java
+++ src/main/java/com/bezkoder/spring/thymeleaf/entity/User.java
@@ -93,7 +93,7 @@ public class User {
 
   @Override
   public String toString() {
-    return "Tutorial [id=" + id + ", uuid=" + code + ", vorname=" + vorname + ", nachname=" + nachname + ", postennummer=" + postennummer + ", start=" + start + ", stop=" + stop + ", fertig=" + fertig + "]";
+    return "User [id=" + id + ", uuid=" + code + ", vorname=" + vorname + ", nachname=" + nachname + ", postennummer=" + postennummer + ", start=" + start + ", stop=" + stop + ", fertig=" + fertig + "]";
   }
 
 }
