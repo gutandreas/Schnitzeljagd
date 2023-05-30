5ad43891419006268c0b8d9aed1ca790c3d22ac4
Andreas Gut <andreasgut@gmx.ch>
2023-05-30 12:28:58 +0200

    Weitere Encrypted Keys eingefügt, verschiedene Modi für Fragen können eingestellt werden

--- src/main/java/com/bezkoder/spring/thymeleaf/QuestionList.java
+++ src/main/java/com/bezkoder/spring/thymeleaf/QuestionList.java
@@ -7,10 +7,31 @@ public class QuestionList {
     private static final HashMap<Integer, Question> questionMap = new HashMap<>();
     private static final HashMap<String, Integer> encryptionMap = new HashMap<>();
     static {
-        questionMap.put(1, new Question("turnhalle","Wie heisst Einstein mit Vorname?", new String[]{"Albert"}, "Er beginnt mit 'A'", "Haupteingang"));
-        questionMap.put(2, new Question("haupteingang", "Wie heisst der Rektor mit Vorname?", new String[]{"Ueli", "Ulrich"}, "Er beginnt mit 'U'", "Sekretariat"));
-        questionMap.put(3, new Question("sekretariat","Wie heisst Bundesrat Berset mit Vorname?", new String[]{"Alain"}, "Er beginnt mit 'A'", "Aula"));
+        addQuestionsToMap(2);
+        addEncryptedKeysToMap();
+    }
 
+    public static void addQuestionsToMap(int modus){
+
+        switch (modus){
+            case 1:
+                System.out.println("Modus Kantifest gestartet");
+                questionMap.put(1, new Question("turnhalle","Wie heisst der Physiker Einstein mit Vorname?", new String[]{"Albert"}, "Er beginnt mit 'A'", "Haupteingang"));
+                questionMap.put(2, new Question("haupteingang", "Wie heisst der Rektor mit Vorname?", new String[]{"Ueli", "Ulrich"}, "Er beginnt mit 'U'", "Sekretariat"));
+                questionMap.put(3, new Question("sekretariat","Wie heisst Bundesrat Berset mit Vorname?", new String[]{"Alain"}, "Er beginnt mit 'A'", "Aula"));
+                break;
+            case 2:
+                System.out.println("Modus G21s gestartet");
+                questionMap.put(1, new Question("turnhalle","Wie heisst Trump mit Vorname?", new String[]{"Donald"}, "Er beginnt mit 'D'", "Haupteingang"));
+                questionMap.put(2, new Question("haupteingang", "Wie heisst der Rektor mit Vorname?", new String[]{"Ueli", "Ulrich"}, "Er beginnt mit 'U'", "Sekretariat"));
+                questionMap.put(3, new Question("sekretariat","Wie heisst Bundesrat Berset mit Vorname?", new String[]{"Alain"}, "Er beginnt mit 'A'", "Aula"));
+                break;
+        }
+
+
+    }
+
+    public static void addEncryptedKeysToMap(){
         encryptionMap.put("XF98EG", 1);
         encryptionMap.put("HJ34IK", 2);
         encryptionMap.put("FQ90BM", 3);
@@ -23,7 +44,6 @@ public class QuestionList {
         encryptionMap.put("LQ61VK", 10);
         encryptionMap.put("ZU92MX", 11);
         encryptionMap.put("SE70MV", 12);
-
     }
 
     public static Question getQuestionByEncryptedKey(String encryptedKey){
