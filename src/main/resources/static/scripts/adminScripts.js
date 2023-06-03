d00c061d421648645c36e803d3b6c3da498d61ab
Andreas Gut <andreasgut@gmx.ch>
2023-06-03 17:47:58 +0200

    Hashfunktion vereinfacht

--- src/main/resources/static/scripts/adminScripts.js
+++ src/main/resources/static/scripts/adminScripts.js
@@ -1,12 +1,12 @@
 async function deleteAll() {
-    if (await checkPassword()){
+    if (checkPassword()){
         url = "/admin/delete"
         console.log(url)
         fetch(url, {method: "DELETE"})
             .then(response => {
                 if (response.ok) {
                     console.log('Daten erfolgreich gelöscht.');
-                    var url = "/admin/loadPage";
+                    var url = "/admin/load";
                     window.location.href = url;
                 } else {
                     console.error('Fehler beim Löschen der Daten.');
@@ -23,13 +23,13 @@ async function deleteAll() {
 }
 
 async function changeModus(){
-    if (await checkPassword()){
+    if (checkPassword()){
         url = "/admin/changeModus?modus=".concat(document.getElementById("modusText").value)
         fetch(url)
             .then(response => {
                 if (response.ok) {
                     console.log('Modus erfolgreich geändert und alle User gelöscht.');
-                    var url = "/admin/loadPage";
+                    var url = "/admin/load";
                     window.location.href = url;
                 } else {
                     console.error('Fehler beim Ändern des Modus.');
@@ -46,24 +46,21 @@ async function changeModus(){
 
 }
 
-async function checkPassword() {
-
-    const hashedPassword = "6308D330240351DF1A05E686D05C7F5207F6293B73460D1B5065841A003047F6"
-    const encoder = new TextEncoder();
-    const data = encoder.encode(document.getElementById("password").value);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0'))
-    const hashString = hashHex.reduce((result, current) => result + current);
-    const hashUpperCase = hashString.toUpperCase();
-    console.log(hashUpperCase)
 
 
-    if(hashedPassword == hashUpperCase){
-        return true;
+function checkPassword() {
+
+    let correctHash = "OxJxEx?=?>"
+    let input = document.getElementById("password").value;
+    let hashedValue = '';
+
+    for (let i = 0; i < input.length; i++) {
+        const charCode = input.charCodeAt(i);
+        const hashedCharCode = charCode ^ 13;
+
+        hashedValue += String.fromCharCode(hashedCharCode);
     }
-    else {
-        return false
-    }
+
+    return correctHash == hashedValue;
 }
 
